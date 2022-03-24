e8dc12cb632c86d106fd75fe3d26fd254e5ff341
beiluo <beiluo@zmengs.com>
2022-03-24 18:34:17 +0800

    [update] 依赖版本提升

---
+++ paging_ktx/src/main/java/cn/leo/paging_ktx/ext/RecyclerViewExt.kt
@@ -0,0 +1,14 @@
+package cn.leo.paging_ktx.ext
+
+import androidx.recyclerview.widget.RecyclerView
+import cn.leo.paging_ktx.simple.SimpleHolder
+
+/**
+ * @author : ling luo
+ * @date : 2022/3/24
+ * @description : RecyclerView 拓展
+ */
+
+fun RecyclerView.addHolder(holder: SimpleHolder<*>) {
+
+}
